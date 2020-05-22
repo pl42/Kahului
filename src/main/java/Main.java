b572ab2fd20bf38e873f359f77d12d3e296185da
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-05-23 07:37:58 +0800

    Formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ import mind.Kahului;
 import model.MindData;
 
 public class Main {
-  public static void main(String[] args) {
-    Kahului dolores = new Kahului();
-    Kahului.playSweetWater();
-    MindData theCradle = dolores.gatherData();
-  }
+	public static void main(String[] args) {
+		Kahului dolores = new Kahului();
+		Kahului.playSweetWater();
+		MindData theCradle = dolores.gatherData();
+	}
 }

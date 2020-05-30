b9c16183199afcb2673d164d44e85e361035ec00
davidad <davidad@alum.mit.edu>
2020-05-30 16:55:39 +0800

    Added class to help with data prediction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import mind.Kahului;
-import model.data.MindData;
-import model.data.PredictionData;
 
 public class Main {
 	public static void main(String[] args) {
 		Kahului dolores = new Kahului();
 		//Kahului.playSweetWater();
-		MindData theCradle = dolores.gatherData();
-		PredictionData predictionData = new PredictionData(theCradle);
-		predictionData.calculatePredictionData();
+		dolores.gatherMindData();
+		dolores.gatherPredictionData();
+
 	}
 }

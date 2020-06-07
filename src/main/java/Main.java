2d782109666f7e0b31002aa65f9def7772dff595
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-06-07 12:03:24 +0800

    Stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,27 @@
+import com.binance.api.client.domain.market.CandlestickInterval;
 import mind.Kahului;
+import model.DataIdentifier;
+import utils.CalcUtils;
+
+import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args) {
-		Kahului dolores = new Kahului();
+
+		if(args[0] == null || args[1] == null) System.exit(-1);
+		if(args[0].equals("") || args[1].equals("")) System.exit(-2);
+
 		Kahului.playSweetWater();
-		dolores.gatherMindData();
-		dolores.gatherPredictionData();
+
+		for(;;) {
+			Kahului dolores = new Kahului(args[0], args[1]);
+			dolores.gatherMindData();
+			dolores.gatherPredictionData();
+			if (dolores.predictionData.sellConfidencePercentage > 85.0) {
+				dolores.performSellAndBuyBack();
+			} else {
+				new CalcUtils().sleeper(30000);
+			}
+		}
 	}
 }

f3ecfeeec6e423b157896d66d8c5e02a6f70ccdd
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-06-27 23:56:35 +0800

    Now trades more BTC if it can and logs more

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		if (args[0] == null || args[1] == null) System.exit(-1);
 		if (args[0].equals("") || args[1].equals("")) System.exit(-2);
 		//Kahului.playSweetWater();
+		System.out.println("Starting KAHULUI version 1.0.2 ...");
 		for (; ; ) {
 			Kahului dolores = new Kahului(args[0], args[1]);
 			dolores.gatherMindData();

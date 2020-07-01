e457220b994428d650e5ebd14f4d7049a483da10
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-07-01 18:22:42 +0800

    Started using log4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import mind.Kahului;
+import org.apache.log4j.Logger;
 import utils.CalcUtils;
 
 public class Main {
+
+	final static Logger logger = Logger.getLogger(Kahului.class);
+
 	public static void main(String[] args) {
 		if (args[0] == null || args[1] == null) System.exit(-1);
 		if (args[0].equals("") || args[1].equals("")) System.exit(-2);
 		//Kahului.playSweetWater();
-		System.out.println("Starting KAHULUI version 1.0.2 ...");
+		logger.info("Starting KAHULUI version 1.0.2 ...");
 		for (; ; ) {
 			Kahului dolores = new Kahului(args[0], args[1]);
 			dolores.gatherMindData();

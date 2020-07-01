ed1809a32c3f3d6910b5ead46628ab8e8e42ccd0
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-07-02 01:51:22 +0800

    Log changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,20 @@ import org.apache.log4j.Logger;
 import utils.CalcUtils;
 
 public class Main {
-
 	final static Logger logger = Logger.getLogger(Kahului.class);
 
 	public static void main(String[] args) {
-		if (args[0] == null || args[1] == null) System.exit(-1);
-		if (args[0].equals("") || args[1].equals("")) System.exit(-2);
-		//Kahului.playSweetWater();
 		logger.info("Starting KAHULUI version 1.0.2 ...");
+		if (args[0] == null || args[1] == null) {
+			System.exit(-1);
+			logger.error("Not enough arguments have been given");
+		}
+		if (args[0].equals("") || args[1].equals("")) {
+			System.exit(-2);
+			logger.error("Arguments cannot be empty");
+		}
+		logger.error("Starting KAHULUI version 1.0.2 ...");
+		//Kahului.playSweetWater();
 		for (; ; ) {
 			Kahului dolores = new Kahului(args[0], args[1]);
 			dolores.gatherMindData();

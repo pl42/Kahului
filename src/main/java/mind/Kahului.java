0dd67ebf0fc84c59afb3e3070252aeb6a67c1451
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-10-17 14:28:17 +0800

    Uncomment important stuff

--- src/main/java/mind/Kahului.java
+++ src/main/java/mind/Kahului.java
@@ -236,9 +236,9 @@ public class Kahului {
 			//WE SHOULD SELL AND BUY!
 			String message = "Deciding to sell! Current: $" + lastPriceFloored + " Target: $" + target + " Buy back: $" + buyBack;
 			logger.info(message);
-			//sendTweet(message);
+			sendTweet(message);
 			//My bad I was sending a tweet
-			//performSellAndBuyBack(lastPriceFloored, buyBack);
+			performSellAndBuyBack(lastPriceFloored, buyBack);
 		}
 	}
 

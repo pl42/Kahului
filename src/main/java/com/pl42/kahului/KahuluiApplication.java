d1f159d767c5295bf457aa0433afd8bdfaf9bd5e
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-12-08 05:49:59 +0800

    Formatting

--- src/main/java/com/pl42/kahului/KahuluiApplication.java
+++ src/main/java/com/pl42/kahului/KahuluiApplication.java
@@ -10,14 +10,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class KahuluiApplication {
   private final static Logger logger = Logger.getLogger(KahuluiApplication.class);
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     logger.info("Starting KAHULUI (v5.1.7) ...");
     if (args.length < 6) {
       logger.error("Not enough arguments have been given");
       System.exit(-1);
     }
-		ConfigurableApplicationContext context = SpringApplication.run(KahuluiApplication.class, args);
+    ConfigurableApplicationContext context = SpringApplication.run(KahuluiApplication.class, args);
     Kahului kahului = context.getBean(Kahului.class);
     kahului.startTrading();
-	}
+  }
 }

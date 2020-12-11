cfbbd0adb91c5feddd7dba8f401ad8ff06a0f117
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-12-11 23:21:08 +0800

    Added endpoint and moved main Spring App

---
+++ src/main/java/com/pl42/KahuluiApplication.java
@@ -0,0 +1,30 @@
+package com.pl42;
+
+import com.pl42.kahului.mind.Kahului;
+import com.pl42.kahului.utils.CalcUtils;
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahuluiApplication {
+  private final static Logger logger = Logger.getLogger(KahuluiApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(KahuluiApplication.class, args);
+    logger.info("Starting KAHULUI (v6.0.0) ...");
+    if (args.length < 6) {
+      logger.error("Not enough arguments have been given");
+      System.exit(-1);
+    }
+    logger.info("Starting Kahului trading...");
+    for (;;) {
+      Kahului dolores = new Kahului(args[0], args[1]);
+      dolores.setTwitterCreds(args[2], args[3], args[4], args[5]);
+      dolores.gatherMindData();
+      dolores.predictAndTrade();
+      dolores.printBalances();
+      new CalcUtils().sleeper(25000);
+    }
+  }
+}

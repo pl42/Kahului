47d6c1cd6b541426395c13d5855f1aa6b3487ef0
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-12-12 17:02:49 +0800

    Attempting to do Autowiring things

--- src/main/java/com/pl42/controller/KahuluiController.java
+++ src/main/java/com/pl42/controller/KahuluiController.java
@@ -1,5 +1,7 @@
 package com.pl42.controller;
 
+import com.pl42.kahului.mind.Kahului;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KahuluiController {
+  @Autowired
+  private Kahului dolores;
 
   @RequestMapping(path = "/totalBalance", method = RequestMethod.GET)
   public ResponseEntity getTotalBalance() {
-    return new ResponseEntity("HI", HttpStatus.OK);
+
+    return new ResponseEntity(dolores.getTotalBalance(), HttpStatus.OK);
   }
 }

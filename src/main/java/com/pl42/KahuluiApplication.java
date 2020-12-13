fa00c120f9f686ac2debed87e2e9a3649f467e90
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-12-14 07:34:31 +0800

    Basically, shit works now

--- src/main/java/com/pl42/KahuluiApplication.java
+++ src/main/java/com/pl42/KahuluiApplication.java
@@ -3,15 +3,17 @@ package com.pl42;
 import com.pl42.kahului.mind.Kahului;
 import com.pl42.kahului.utils.CalcUtils;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KahuluiApplication {
   private final static Logger logger = Logger.getLogger(KahuluiApplication.class);
 
   public static void main(String[] args) {
-    SpringApplication.run(KahuluiApplication.class, args);
+    ConfigurableApplicationContext context = SpringApplication.run(KahuluiApplication.class, args);
     logger.info("Starting KAHULUI (v6.0.0) ...");
     if (args.length < 6) {
       logger.error("Not enough arguments have been given");
@@ -19,7 +21,8 @@ public class KahuluiApplication {
     }
     logger.info("Starting Kahului trading...");
     for (;;) {
-      Kahului dolores = new Kahului(args[0], args[1]);
+      Kahului dolores = context.getBean(Kahului.class);
+      dolores.setBinanceCreds(args[0], args[1]);
       dolores.setTwitterCreds(args[2], args[3], args[4], args[5]);
       dolores.gatherMindData();
       dolores.predictAndTrade();

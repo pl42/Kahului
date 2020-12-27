faeffafb14314b737c0a450c5d73f9ba61e78488
Peggy Liang <63401571+PL42@users.noreply.github.com>
2020-12-27 18:48:56 +0800

    FoodBack

--- src/main/java/com/pl42/kahului/mind/Kahului.java
+++ src/main/java/com/pl42/kahului/mind/Kahului.java
@@ -31,7 +31,7 @@ import static com.binance.api.client.domain.account.NewOrder.*;
 
 @Service
 public class Kahului {
-  private static final boolean DEVELOPING = true;
+  private static final boolean DEVELOPING = false;
   private static final Logger logger = Logger.getLogger(Kahului.class);
   private static CandlestickInterval[] intervalList = {
     CandlestickInterval.ONE_MINUTE, CandlestickInterval.THREE_MINUTES,

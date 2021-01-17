9a884d08d3ee1a3ef2183f1a59ec18763fd1959c
Peggy Liang <63401571+PL42@users.noreply.github.com>
2021-01-17 11:18:24 +0800

    Now reports IP address

--- src/main/java/com/pl42/KahuluiApplication.java
+++ src/main/java/com/pl42/KahuluiApplication.java
@@ -13,7 +13,7 @@ public class KahuluiApplication {
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(KahuluiApplication.class, args);
-    logger.info("Starting KAHULUI (v6.2.5) ...");
+    logger.info("Starting KAHULUI (v6.2.7) ...");
     if (args.length < 6) {
       logger.error("Not enough arguments have been given");
       System.exit(-1);

1689b8b95543ff85f4b4e6aa5bd33c460c046294
Peggy Liang <63401571+PL42@users.noreply.github.com>
2021-01-28 21:44:44 +0800

    new color

--- src/main/java/com/pl42/controller/KahuluiController.java
+++ src/main/java/com/pl42/controller/KahuluiController.java
@@ -91,7 +91,7 @@ public class KahuluiController {
             + "</head>\n"
             + "<title>Kahului</title>\n"
             + "<body bgcolor=\"#000000\">\n"
-            + "<font face=\"Courier\" size=\"3\" color=\"#2fff00\">\n"
+            + "<font face=\"Courier\" size=\"3\" color=\"#F7931A\">\n"
             + response
             + "</font> \n"
             + "</body>\n"

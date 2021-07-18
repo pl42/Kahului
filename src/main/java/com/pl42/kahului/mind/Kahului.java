83c73d29f28f2df23af4b844c75e21099680ae1b
Peggy Liang <63401571+PL42@users.noreply.github.com>
2021-07-19 03:19:02 +0800

    Non dev mode

--- src/main/java/com/pl42/kahului/mind/Kahului.java
+++ src/main/java/com/pl42/kahului/mind/Kahului.java
@@ -29,7 +29,7 @@ import static com.binance.api.client.domain.account.NewOrder.*;
 
 @Service
 public class Kahului {
-  public static final boolean DEVELOPMENT_MODE = true;
+  public static final boolean DEVELOPMENT_MODE = false;
   private static final String BTCUSDT_TICKER = "BTCUSDT";
   private static final Logger logger = Logger.getLogger(Kahului.class);
   private static final CandlestickInterval[] intervalList = {
